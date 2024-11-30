75607f01d83ca32d0d673275a1b9d0b374617107
taniarani <40597746+taniarani@users.noreply.github.com>
2024-11-30 15:31:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let panels = document.querySelectorAll('.panel');
+
+panels.forEach(x => {
+    x.addEventListener('click', () => {
+        removeActiveClasses();
+        x.classList.add('active');
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(x => {
+        x.classList.remove('active');
+    });
+}
